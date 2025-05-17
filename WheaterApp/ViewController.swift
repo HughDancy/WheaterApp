3629fb26266aeebddb202b1b7dacfbb2c1db1327
boris.k <boris.k@appio.team>
2025-05-17 14:57:24 +0700

    Initial Commit

---
+++ WheaterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheaterApp
+//
+//  Created by Борис Киселев on 17.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WheaterAppTests/WheaterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterAppTests.swift
+//  WheaterAppTests
+//
+//  Created by Борис Киселев on 17.05.2025.
+//
+
+import Testing
+@testable import WheaterApp
+
+struct WheaterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
